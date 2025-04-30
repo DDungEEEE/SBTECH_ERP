7e4daf620232082b859797b30314e3ddf0bd1492
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-04-30 17:45:56 +0900

    feat: Filter 딴에서 json 응답을 위한 ResponseWrapper 작성

---
+++ sbtech-erp/src/main/java/com/sbtech/erp/util/ResponseWrapper.java
@@ -0,0 +1,24 @@
+package com.sbtech.erp.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class ResponseWrapper {
+    private final ObjectMapper objectMapper;
+
+    public void convertObjectToResponse(HttpServletResponse res, Object object) throws IOException {
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+
+        String result = objectMapper.writeValueAsString(object);
+
+        res.getWriter().write(result);
+    }
+}
+
