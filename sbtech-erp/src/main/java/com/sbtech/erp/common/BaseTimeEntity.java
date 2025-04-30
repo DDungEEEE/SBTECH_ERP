a64ee03587bf9f2d5c98d39792763c905c460ac3
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-04-30 16:43:17 +0900

    feat: 생성 시간,수정 시간을 설정할 공통 엔티티 생성

---
+++ sbtech-erp/src/main/java/com/sbtech/erp/common/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.sbtech.erp.common;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@MappedSuperclass
+public abstract class BaseTimeEntity implements Serializable {
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    protected Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    protected  Timestamp updatedAt;
+
+}
