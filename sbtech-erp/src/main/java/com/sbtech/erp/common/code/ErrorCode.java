a76dfa53e977143df32d61e26f83110e39f7c040
손원익 <102235796+DDungEEEE@users.noreply.github.com>
2025-05-09 14:51:49 +0900

    refactor: 공통 에러코드 업데이트

--- sbtech-erp/src/main/java/com/sbtech/erp/common/code/ErrorCode.java
+++ sbtech-erp/src/main/java/com/sbtech/erp/common/code/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    INTERNAL_SERVER_ERROR(500, "COMMON-001", "SERVER ERROR");
+    INTERNAL_SERVER_ERROR(500, "COMMON_001", "SERVER ERROR"),
+    DUPLICATED_DEPARTMENT_ERROR(409, "DEPARTMENT_001", "Duplicated Department Name Error");
     private final int status;
     private final String divisionCode;
     private final String reason;
