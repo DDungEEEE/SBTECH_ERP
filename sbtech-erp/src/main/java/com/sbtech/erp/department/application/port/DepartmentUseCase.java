4e1f8d53fa22576369f2fb455ae5bf0e3cb2076b
손원익 <102235796+DDungEEEE@users.noreply.github.com>
2025-05-09 14:52:49 +0900

    feat: 부서 Usecase작성

--- sbtech-erp/src/main/java/com/sbtech/erp/department/application/port/DepartmentUseCase.java
+++ sbtech-erp/src/main/java/com/sbtech/erp/department/application/port/DepartmentUseCase.java
@@ -1,4 +1,11 @@
 package com.sbtech.erp.department.application.port;
 
+import com.sbtech.erp.department.adapter.in.dto.DepartmentCreateDto;
+import com.sbtech.erp.department.domain.Department;
+
+import java.util.List;
+
 public interface DepartmentUseCase {
+    Department create(DepartmentCreateDto dto);
+    List<Department> getSubDepartments(Long departmentId);
 }
