0e4c0e8f432f9382bb296edfa0bf702edcc5a005
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-05-13 17:36:14 +0900

    refactor: 스웨거에 Authorization 헤더 추가

--- sbtech-erp/src/main/java/com/sbtech/erp/common/config/SwaggerConfig.java
+++ sbtech-erp/src/main/java/com/sbtech/erp/common/config/SwaggerConfig.java
@@ -1,17 +1,47 @@
 package com.sbtech.erp.common.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SwaggerConfig{
+public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi jwtApi(){
+    public GroupedOpenApi api(){
         return GroupedOpenApi.builder()
                 .group("public")
                 .pathsToMatch("/**")
+                .addOpenApiCustomizer(openApi -> {
+                    openApi.getComponents().addParameters("Authorization", createHeader("Authorization"));
+                })
                 .build();
+    }
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info().title("Dongguk Chatting API DOCUMENT")
+                        .description("Dongguk Chatting API입니다.")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+    private Parameter createHeader(String name){
+        return new Parameter()
+                .in("header")
+                .schema(new io.swagger.v3.oas.models.media.StringSchema())
+                .name(name)
+                .description(name)
+                .required(false);
     }
 }
