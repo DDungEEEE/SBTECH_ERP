7fa75dc7360635e66b5278d28f07b2d7669276a1
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-05-13 17:36:02 +0900

    Update ErrorCode.java

--- sbtech-erp/src/main/java/com/sbtech/erp/common/code/ErrorCode.java
+++ sbtech-erp/src/main/java/com/sbtech/erp/common/code/ErrorCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "COMMON_001", "SERVER ERROR"),
     DUPLICATED_DEPARTMENT_ERROR(409, "DEPARTMENT_001", "Duplicated Department Name Error");
+
     private final int status;
     private final String divisionCode;
     private final String reason;
