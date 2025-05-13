ae09242c338599d981c91a71b2f769c40e175f8a
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-05-13 17:35:43 +0900

    chore: jackson 라이브러리 추가

--- sbtech-erp/src/main/java/com/sbtech/erp/auth/adapter/out/repository/JpaSystemRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sbtech.erp.auth.adapter.out.repository;
-
-import com.sbtech.erp.auth.domain.role.SystemRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaSystemRoleRepository extends JpaRepository<SystemRole, Long> {
-}
