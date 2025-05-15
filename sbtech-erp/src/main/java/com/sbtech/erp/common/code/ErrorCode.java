4a6a8a78fd794807c18983ae532063af06874782
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-05-15 17:38:07 +0900

    refactor: 에러 코드 추가(접근 거부, 토큰 오류 코드)

--- sbtech-erp/src/main/java/com/sbtech/erp/common/code/ErrorCode.java
+++ sbtech-erp/src/main/java/com/sbtech/erp/common/code/ErrorCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "COMMON_001", "SERVER ERROR"),
-    DUPLICATED_DEPARTMENT_ERROR(409, "DEPARTMENT_001", "Duplicated Department Name Error");
+    DUPLICATED_DEPARTMENT_ERROR(409, "DEPARTMENT_001", "Duplicated Department Name Error"),
+    INVALID_TOKEN_ERROR(401, "AUTH_001", "유효하지 않은 토큰입니다."),
+    NO_PERMISSION_ERROR(401, "AUTH_002", "해당 리소스에 대한 권한이 없습니다.");
 
     private final int status;
     private final String divisionCode;
