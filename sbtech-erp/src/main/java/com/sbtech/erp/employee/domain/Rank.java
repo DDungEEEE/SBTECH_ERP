4ba4177c316c08a334f1495a45f534e03269489e
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-05-16 18:20:34 +0900

    refactor: level 삭제

--- sbtech-erp/src/main/java/com/sbtech/erp/employee/domain/Rank.java
+++ sbtech-erp/src/main/java/com/sbtech/erp/employee/domain/Rank.java
@@ -6,22 +6,16 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Rank {
-    INTERN("인턴", 0),
-    STAFF("사원", 1),
-    ASSISTANT("주임", 2),
-    SENIOR("대리", 3),
-    MANAGER("과장", 4),
-    DIRECTOR("차장", 5),
-    LEAD("부장", 6),
-    EXECUTIVE("이사", 7);
+    INTERN("인턴"),
+    STAFF("사원"),
+    ASSISTANT("주임"),
+    SENIOR("대리"),
+    MANAGER("과장"),
+    DIRECTOR("차장"),
+    LEAD("부장"),
+    EXECUTIVE("이사");
 
     private final String label;
-
-    private final int level;
-
-    public boolean isAtLeast(Rank other){
-        return this.level >= other.level;
-    }
 
     public static Rank from(String rank){
         return Rank.valueOf(rank.toUpperCase());
